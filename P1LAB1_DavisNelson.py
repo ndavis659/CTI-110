bd417555f7e5b766ce79956b22635a810e2ffd03
ndavis659 <davisn3401@student.faytechcc.edu>
2026-09-01 19:13:20 -0400

    Add files via upload

---
+++ P1LAB1_DavisNelson.py
@@ -0,0 +1,9 @@
+#first name: Nelson
+#last name: Davis
+#Hello,Nelson Davis! Welcome to CTI-110
+
+first_name = input ("Enter you first name: ")
+last_name = input ("Enter you last name: ")
+
+print("Hello,", first_name, last_name + "!","Welcome to CTI-110")
+
